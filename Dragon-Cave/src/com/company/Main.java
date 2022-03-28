696314d9bb759e1928bacf279451316b577c5393
Yoseph <yosephfl@gmail.com>
2022-03-28 12:07:24 -0400

    added try/catch

--- Dragon-Cave/src/com/company/Main.java
+++ Dragon-Cave/src/com/company/Main.java
@@ -12,17 +12,18 @@ public class Main {
         System.out.println("and will share his treasure with you. The other dragon");
         System.out.println("is greedy and hungry and will eat you on sight.");
         System.out.println("Which cave will you go into? (1 or 2)");
-        int choice = myObj.nextInt();
-        switch (choice) {
-            case 1:
-                System.out.println("You approach the cave...");
-                System.out.println("It is dark and spooky...");
-                System.out.println("A large dragon jumps out in front of you! He opens his jaws and...");
-                System.out.println("Gobbles you down in one bite!");
-        }
 
+        try{ int choice = myObj.nextInt();
 
-
-            return;
+            switch (choice) {
+                case 1:
+                    System.out.println("You approach the cave...");
+                    System.out.println("It is dark and spooky...");
+                    System.out.println("A large dragon jumps out in front of you! He opens his jaws and...");
+                    System.out.println("Gobbles you down in one bite!");
+            }
+        } catch (Exception ex) {
+            System.out.println("Please re-enter a viable input.");}
+        return;
     }
 }
